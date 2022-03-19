4434b09d8f60f063f5a20c0481364f6334671b44
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-03-19 07:33:58 +0100

    my solution

--- diamond.py
+++ diamond.py
@@ -1,2 +1,17 @@
+import string
+
+
 def rows(letter):
-    pass
+    """produces diamond"""
+    letters = string.ascii_uppercase
+    num = letters.index(letter)
+    result = []
+    result.append(" " * num + "A" + " " * num)
+
+    for i, char in enumerate(letters[1: num + 1]):
+        result.append(
+            " " * (num - i - 1) + char + " " *
+            (i * 2 + 1) + char + " " * (num - i - 1)
+        )
+
+    return result + [res for res in result[-2::-1]]
