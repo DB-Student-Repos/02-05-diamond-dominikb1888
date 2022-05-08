22bcb30ba8dca4a285682484dd99f052b2e297e9
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-05-08 22:30:31 +0200

    slight consistency change on whitespace addition, now f-string too!

--- diamond.py
+++ diamond.py
@@ -5,7 +5,7 @@ def rows(char):
     a = string.ascii_uppercase
     n = a.index(char)
     return [
-        f"{a[j]:>{i}}{a[j] if j>0 else '':>{j*2}}" + " " * (i - 1)
+        f"{a[j]:>{i}}{a[j] if j>0 else '':>{j*2}}{' '*(i-1)}"
         for j, i in zip(
             list(range(n + 1)) + list(reversed(range(0, n))),
             list(range(n + 1, 1, -1)) + list(reversed(range(n + 1, 0, -1))),
